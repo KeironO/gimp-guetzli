f40ee8639971232cd2442ab47939221b596c7ddc
KeironO <keiron.oshea@gmail.com>
2019-05-30 19:10:05 +0100

    Woo

--- src/guetzli-export.py
+++ src/guetzli-export.py
@@ -1,26 +1,49 @@
 #!/usr/bin/env python
 
-
 from gimpfu import *
 import os
+from tempfile import NamedTemporaryFile as ntf
 
-print(os.environ)
+try:
+    guetzli = os.environ["GUETZLI"]
+except KeyError:
+    gimp.message("Unable to find GUETZLI in your environment path, have you set it up correctly?")
 
-def do_export(image, drawable, filename, raw_filename,quality, smoothing, ringing_reduction):
+
+def create_tmpfile_fp():
+    tmpfile = ntf(suffix=".png")
+    tmpfile_fp = tmpfile.name
+    tmpfile.close()
+    return tmpfile_fp
+ 
+
+def do_export(image, drawable, filename, raw_filename, quality):
+
     gimp.progress_init("Exporting %s" % raw_filename)
 
+    args = [guetzli]
+
+    args.append("--quality %" % str(quality))
 
 
 def register_save():
-    gimp.register_save_handler("guetzli-save", "jpeg", "jpg")
+    gimp.register_save_handler("guetzli-save", "jpeg", "jpg", "")
 
 register (
-    "save-guetzli",
-    "Export via Google's Perceptual JPEG encoder",
-    "Export via Google's Perceptual JPEG encoder",
-    "Keiron O'Shea",
-    "Keiron O'Shea",
-    "2019"
+    proc_name="save-guetzli",
+    blurb="Export via Google's Perceptual JPEG encoder",
+    help="Export via Google's Perceptual JPEG encoder",
+    author="Keiron O'Shea",
+    copyright="MIT",
+    date="2019",
+    label="<Save>/JPEG (Guetzli)",
+    imagetypes="RGB*, GRAY*",
+    params=[
+        (PF_SLIDER, "quality", "Quality", 90, (80, 100, 1))    
+    ],
+    results=[],
+    function=do_export,
+    on_query=register_save
 )
 
 
